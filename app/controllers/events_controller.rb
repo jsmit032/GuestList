ed0a2deb7de916778e0228af982bc3be02258fc6
jsmit032 <jsmit032@ucr.edu>
2014-11-06 10:11:19 -0800

    added new events page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,28 @@ class EventsController < ApplicationController
 	def index
 		@events = Event.all
 	end
+
+	def new
+		@event = Event.new
+	end
+
+	def create
+		@event = Event.new(event_params)
+		if @event.save
+			redirect_to events_path
+		else
+			render 'new'
+		end
+	end
+
+	private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_event
+      @event = Event.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def event_params
+      params.require(:event).permit(:name, :location, :date, :time, :venue, :venue_address)
+    end
 end
