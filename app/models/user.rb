4bcfdae3088c4189caf7d59ae1cedc8e7c1d5e35
jsmit032 <jsmit032@ucr.edu>
2014-11-09 22:55:44 -0800

    add styling to tables

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User
   include Mongoid::Document
   include ActiveModel::SecurePassword
+  field :first_name, type: String
+  field :last_name, type: String
   field :email, type: String
   field :password_digest, type: String
 
